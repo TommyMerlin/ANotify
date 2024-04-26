feb6485896ba8c8db9efcc79f3eb951f4f4c9331
HuYe <zjuhuye@zju.edu.cn>
2024-04-26 21:36:25 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='anotify',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(),
     install_requires=[
         'requests>=2.25.1',
@@ -10,7 +10,7 @@ setup(
     author='7ommy',
     author_email='tommymerlin0920@gmail.com',
     description='Send notifications by multiple channels.',
-    long_description=open('README.md').read(),
+    long_description=open('README.md', encoding='utf-8').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/TommyMerlin/ANotify',
     license='MIT',
