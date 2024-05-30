8ac1e96a14e1a860f42c2603cb008e53e6b2a9b4
HuYe <zjuhuye@zju.edu.cn>
2024-05-30 10:22:03 +0800

    微信发送消息新增 Markdown

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='anotify',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'requests>=2.15.1',
