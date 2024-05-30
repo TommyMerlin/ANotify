c27d69d91e1ed5dbd5d6f04518aaee7cd571f222
HuYe <zjuhuye@zju.edu.cn>
2024-05-30 12:03:35 +0800

    Update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='anotify',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     install_requires=[
         'requests>=2.15.1',
